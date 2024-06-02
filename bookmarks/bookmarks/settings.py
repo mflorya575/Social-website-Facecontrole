4a83e9effc825d7ba05d63ec56709001cc6d1509
Michael Florya <mih2303@mail.ru>
2024-06-02 19:25:07 +0300

    add new backend auth for users

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -106,6 +106,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend',
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
